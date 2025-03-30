b60dccd6f27d71a07a473b9028aeab45970deb1c
unknown <evincelmay@gmail.com>
2025-03-30 09:38:38 -1000

    Move files to root for GitHub Pages

---
+++ build/precache-manifest.7e039fc6e3b0b1bd4020ac201f902b90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3bc15655ebd1fd9f9726f77a611d711",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81bf6639d612e935e1a0",
+    "url": "/static/css/main.49ca8e89.chunk.css"
+  },
+  {
+    "revision": "91532fe74c9ded204142",
+    "url": "/static/js/2.a7f37d0f.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "81bf6639d612e935e1a0",
+    "url": "/static/js/main.8bc4bac1.chunk.js"
+  },
+  {
+    "revision": "cd2988c65fcca3ce3217",
+    "url": "/static/js/runtime~main.b45b9353.js"
+  }
+]);
